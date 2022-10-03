697522e568a7330f63d274925ebaa71b916fd989
rusomalevich <ezequiel.glik@gmail.com>
2022-10-03 08:52:59 -0300

    Agrega archivos de imágenes para maquetado. Agrega estructura header

---
+++ carrousel.php
@@ -0,0 +1,35 @@
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="carousel-caption d-none d-md-block">
+                <p class="item-number">01</p>
+                <h5>Resoluciones para las Paritarias 2020</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh faucibus leo dolor mattis et venenatis eget a. Nunc mauris cras porttitor hendrerit placerat.</p>
+                <a href="#" title="#" class="link">Ver más</a>
+            </div>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/img-header.jpg" class="d-block" alt="...">
+        </div>
+        <div class="carousel-item">
+            <div class="carousel-caption d-none d-md-block">
+                <p class="item-number">02</p>
+                <h5>Resoluciones para las Paritarias 2020</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh faucibus leo dolor mattis et venenatis eget a. Nunc mauris cras porttitor hendrerit placerat.</p>
+                <a href="#" title="#" class="link">Ver más</a>
+            </div>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/img-header.jpg" class="d-block" alt="...">
+        </div>
+        <div class="carousel-item">
+            <div class="carousel-caption d-none d-md-block">
+                <p class="item-number">03</p>
+                <h5>Resoluciones para las Paritarias 2020</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh faucibus leo dolor mattis et venenatis eget a. Nunc mauris cras porttitor hendrerit placerat.</p>
+                <a href="#" title="#" class="link">Ver más</a>
+            </div>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/img-header.jpg" class="d-block" alt="...">
+        </div>
+    </div>
